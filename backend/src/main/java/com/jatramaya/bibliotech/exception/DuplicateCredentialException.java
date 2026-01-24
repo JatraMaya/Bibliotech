12589624098b01e7c859f0714d16f0bd867b7397
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-24 15:59:44 +0800

    Add Duplicate Entity Exception

---
+++ backend/src/main/java/com/jatramaya/bibliotech/exception/DuplicateCredentialException.java
@@ -0,0 +1,8 @@
+package com.jatramaya.bibliotech.exception;
+
+public class DuplicateCredentialException extends RuntimeException {
+
+    public DuplicateCredentialException(String message) {
+        super(message);
+    }
+}
