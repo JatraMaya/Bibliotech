fcad96c2df749a352e8478c634345cddc3367c1b
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-27 17:29:19 +0800

    Refactor for readability

--- backend/src/main/java/com/jatramaya/bibliotech/controller/UserController.java
+++ backend/src/main/java/com/jatramaya/bibliotech/controller/UserController.java
@@ -3,15 +3,21 @@ package com.jatramaya.bibliotech.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jatramaya.bibliotech.dto.AddProfileDTO;
 import com.jatramaya.bibliotech.dto.CurrentUserResponseDTO;
 import com.jatramaya.bibliotech.entity.user.UserEntity;
+import com.jatramaya.bibliotech.service.AuthService;
 import com.jatramaya.bibliotech.service.UserService;
+
+import jakarta.validation.Valid;
+
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/user")
@@ -20,30 +26,23 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private AuthService auth;
+
     @GetMapping("/me")
     public ResponseEntity<CurrentUserResponseDTO> getCurrentUser() {
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = auth.getName();
-        String fullname;
-
+        String username = auth.getCurrentUsername();
         UserEntity user = service.getCurrentUser(username);
-        if (user.getLastname() != null) {
-            fullname = user.getFirstname() + " " + user.getLastname();
-        } else {
-            fullname = user.getFirstname();
-        }
-        CurrentUserResponseDTO response = new CurrentUserResponseDTO();
-        response.setUsername(user.getUsername());
-        response.setFullname(fullname);
-        response.setEmail(user.getEmail());
 
-        if (user.getProfile() != null) {
-            response.setAvatarUrl(user.getProfile().getAvatarUrl());
-            response.setBio(user.getProfile().getBio());
-        }
-
+        CurrentUserResponseDTO response = new CurrentUserResponseDTO(user);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<Map<String,String>> addProfile(@Valid @RequestBody
+    AddProfileDTO dto ) {
+
+    Aut
     }
 
 }
