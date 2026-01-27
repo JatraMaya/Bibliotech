849ba868c6e7e87f643ca0254ace48c46da540af
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-27 11:43:12 +0800

    Add ChangePasswordDTO for controller

---
+++ backend/src/main/java/com/jatramaya/bibliotech/dto/ChangePasswordDTO.java
@@ -0,0 +1,22 @@
+package com.jatramaya.bibliotech.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordDTO {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    private String newPassword;
+
+}
