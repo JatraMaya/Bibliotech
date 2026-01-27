18bd3e955e21626f39fe40ca670462a0e4cf8c86
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-27 10:19:18 +0800

    Add login method to handle login

--- backend/src/main/java/com/jatramaya/bibliotech/service/AuthService.java
+++ backend/src/main/java/com/jatramaya/bibliotech/service/AuthService.java
@@ -6,30 +6,48 @@ import org.springframework.stereotype.Service;
 
 import com.jatramaya.bibliotech.entity.user.UserEntity;
 import com.jatramaya.bibliotech.exception.DuplicateCredentialException;
+import com.jatramaya.bibliotech.exception.InvalidPasswordException;
 import com.jatramaya.bibliotech.repository.UserRepository;
+import com.jatramaya.bibliotech.utils.JWT;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AuthService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repo;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JWT jwt;
+
     public UserEntity register(UserEntity user) {
 
-        if (userRepository.existsByUsername(user.getUsername())) {
+        if (repo.existsByUsername(user.getUsername())) {
             throw new DuplicateCredentialException("Username or email already exist");
         }
-        if (userRepository.existsByEmail(user.getEmail())) {
+        if (repo.existsByEmail(user.getEmail())) {
             throw new DuplicateCredentialException("Username or email already exist");
         }
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
 
-        return userRepository.save(user);
+        return repo.save(user);
 
+    }
+
+    public String login(String username, String password) {
+        UserEntity user = repo.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("Username not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new InvalidPasswordException("Unauthorized password");
+        }
+
+        return jwt.generateToken(user.getUsername(), user.getRole().name());
     }
 
 }
