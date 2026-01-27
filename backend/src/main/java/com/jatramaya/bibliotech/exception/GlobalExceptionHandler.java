29a8535e0c9736ff394911b96e18ae42552b4a2d
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-27 10:04:38 +0800

    Add Invalid Password handler exception

--- backend/src/main/java/com/jatramaya/bibliotech/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/jatramaya/bibliotech/exception/GlobalExceptionHandler.java
@@ -12,27 +12,34 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(DuplicateCredentialException.class)
-    public ResponseEntity<Map<String, String>> handleDuplicateCredential(DuplicateCredentialException ex) {
-        Map<String, String> response = Map.of(
-                "Status", "Error",
-                "Message", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(response);
-    }
+        @ExceptionHandler(DuplicateCredentialException.class)
+        public ResponseEntity<Map<String, String>> handleDuplicateCredential(DuplicateCredentialException ex) {
+                Map<String, String> response = Map.of(
+                                "Status", "Error",
+                                "Message", ex.getMessage());
+                return ResponseEntity.status(HttpStatus.CONFLICT)
+                                .body(response);
+        }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> handleBadRequest(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+        @ExceptionHandler(InvalidPasswordException.class)
+        public ResponseEntity<Map<String, String>> handleBadPassword(InvalidPasswordException ex) {
+                Map<String, String> response = Map.of("Status", "Error", "Message", ex.getMessage());
 
-        ex.getBindingResult().getFieldErrors()
-                .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
 
-        Map<String, Object> response = Map.of(
-                "status", "error",
-                "message", errors);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(response);
-    }
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, Object>> handleBadRequest(MethodArgumentNotValidException ex) {
+                Map<String, String> errors = new HashMap<>();
+
+                ex.getBindingResult().getFieldErrors()
+                                .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+
+                Map<String, Object> response = Map.of(
+                                "status", "error",
+                                "message", errors);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                .body(response);
+        }
 
 }
