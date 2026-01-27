62001b7bce45c3c7c6ce93780010824db2cd836b
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-27 19:01:10 +0800

    Add new Method for ProfileRepository to look up user by User and check for Duplicate value for profile from Existing user

--- backend/src/main/java/com/jatramaya/bibliotech/repository/ProfileRepository.java
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/ProfileRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jatramaya.bibliotech.entity.user.ProfileEntity;
+import com.jatramaya.bibliotech.entity.user.UserEntity;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<ProfileEntity, UUID> {
     Optional<ProfileEntity> findByUserId(UUID user_id);
+
+    Optional<ProfileEntity> findByUser(UserEntity user);
+
+    boolean existsByUser(UserEntity user);
 }
