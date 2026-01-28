fd6b4ed55345f45588b95600031679b0e6f0df98
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-28 11:45:29 +0800

    Add AdminController layer Add endpoint to delete author based on ID Add endpoint to delete user based on ID Add endpoint to assing/revoke user admin access

---
+++ backend/src/main/java/com/jatramaya/bibliotech/controller/AdminController.java
@@ -0,0 +1,94 @@
+package com.jatramaya.bibliotech.controller;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jatramaya.bibliotech.entity.user.UserEntity;
+import com.jatramaya.bibliotech.service.AdminService;
+import com.jatramaya.bibliotech.service.AuthorService;
+import com.jatramaya.bibliotech.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private AuthorService authorService;
+
+    AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @DeleteMapping("/author/{id}")
+    public ResponseEntity<Map<String, String>> deleteAuthor(@PathVariable Long id) {
+
+        authorService.deleteAuthor(id);
+
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "message", "Author data deleted"));
+
+    }
+
+    @PutMapping("/user/{id}/assign")
+    public ResponseEntity<?> assingAdminFromUser(@PathVariable UUID id) {
+
+        UserEntity newAdmin = adminService.assingAdminAccess(id);
+
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "message", "Success Assign user with Admin privilage",
+                "admin", Map.of(
+                        "username", newAdmin.getUsername(),
+                        "firstname", newAdmin.getFirstname(),
+                        "email", newAdmin.getEmail(),
+                        "role", newAdmin.getRole().name()
+
+                )
+
+        ));
+    }
+
+    @PutMapping("/user/{id}/revoke")
+    public ResponseEntity<?> revokeAdminFromUser(@PathVariable UUID id) {
+
+        UserEntity newAdmin = adminService.revokeAdminAccess(id);
+
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "message", "Success revoke admin access",
+                "admin", Map.of(
+                        "username", newAdmin.getUsername(),
+                        "firstname", newAdmin.getFirstname(),
+                        "email", newAdmin.getEmail(),
+                        "role", newAdmin.getRole().name()
+
+                )
+
+        ));
+    }
+
+    @DeleteMapping("/user/{id}/delete")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) {
+        userService.deleteUserDataById(id);
+
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "message", "User deleted successfully"));
+    }
+
+}
