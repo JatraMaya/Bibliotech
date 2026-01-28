10c92430f37b4a6f36f7f0a852824bb1e4621e92
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-28 11:36:01 +0800

    Remove annotationvalidator and move it to DTO instead

--- backend/src/main/java/com/jatramaya/bibliotech/entity/book/AuthorEntity.java
+++ backend/src/main/java/com/jatramaya/bibliotech/entity/book/AuthorEntity.java
@@ -3,7 +3,6 @@ package com.jatramaya.bibliotech.entity.book;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
@@ -11,15 +10,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "authors", indexes = {
-    @Index(name = "idx_authorName", columnList = "name")
+        @Index(name = "idx_authorName", columnList = "name")
 })
 @Data
 @NoArgsConstructor
@@ -30,13 +27,10 @@ public class AuthorEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    @Size(min = 3, max = 100, message = "Author's name must be at least 3 characters long.")
-    @NotBlank(message = "Auhor's name cannot be blank.")
     private String name;
 
     @ManyToMany(mappedBy = "authors")
     private Set<BookEntity> books = new HashSet<>();
 
-    private String authorePictureUrl;
+    private String authorPictureUrl;
 }
