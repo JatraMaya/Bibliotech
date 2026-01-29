0ab6f0c71686881aa88917da189a7d7e25fd92a5
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-29 20:58:37 +0800

    Add Pagination

--- backend/src/main/java/com/jatramaya/bibliotech/repository/TagRepository.java
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/TagRepository.java
@@ -1,5 +1,9 @@
 package com.jatramaya.bibliotech.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,9 @@ import com.jatramaya.bibliotech.entity.book.TagEntity;
 @Repository
 public interface TagRepository extends JpaRepository<TagEntity, Long> {
 
+    Optional<TagEntity> findByName(String name);
+
+    boolean existsByName(String name);
+
+    Page<TagEntity> findAll(Pageable pageable);
 }
