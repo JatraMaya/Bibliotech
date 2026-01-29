494f2897862029b2ecc746bed0fd89a6d78a978a
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-29 10:38:32 +0800

    Increase File upload max size convert all save image to jpg instead of it's original extention

--- backend/src/main/java/com/jatramaya/bibliotech/service/FileUploadService.java
+++ backend/src/main/java/com/jatramaya/bibliotech/service/FileUploadService.java
@@ -1,6 +1,7 @@
 package com.jatramaya.bibliotech.service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,36 +24,37 @@ public class FileUploadService {
     @Autowired
     private String uploadDirectory;
 
-    private static final long MAX_FILE_SIZE = 10 * 1024 * 1024;
+    private static final long MAX_FILE_SIZE = 15 * 1024 * 1024;
     private static final List<String> ALLOWED_TYPES = Arrays.asList("image/jpeg", "image/png", "image/jpg");
     private static final int MAX_WIDTH = 800;
     private static final int MAX_HEIGHT = 800;
     private static final double QUALITY = 0.8;
+    private static final String OUTPUT_FORMAT = "JPEG";
 
     public String uploadAvatar(MultipartFile file) throws IOException {
 
-        if (!file.isEmpty()) {
+        if (file == null || file.getSize() == 0) {
             throw new IllegalArgumentException("File is empty");
         }
 
         if (file.getSize() > MAX_FILE_SIZE) {
-            throw new IllegalArgumentException("Image cannot be large then 10MB.");
+            throw new IllegalArgumentException("Image cannot be large then 15MB.");
         }
 
         if (!ALLOWED_TYPES.contains(file.getContentType())) {
             throw new IllegalArgumentException("Acceptable file format can only be either JPG/JPEG, or PNG");
         }
 
-        String originalFileName = file.getOriginalFilename();
-        String fileExt = originalFileName.substring(originalFileName.lastIndexOf("."));
-        String newFileName = UUID.randomUUID().toString() + fileExt;
-
+        String newFileName = UUID.randomUUID().toString() + "." + OUTPUT_FORMAT;
         Path filePath = Paths.get(uploadDirectory, newFileName);
 
-        BufferedImage originalImage = ImageIO.read(file.getInputStream());
+        InputStream inputStream = file.getInputStream();
+        BufferedImage originalImage = ImageIO.read(inputStream);
+        inputStream.close();
         Thumbnails.of(originalImage)
                 .size(MAX_WIDTH, MAX_HEIGHT)
                 .outputQuality(QUALITY)
+                .outputFormat(OUTPUT_FORMAT)
                 .toFile(filePath.toFile());
 
         return "/uploads/avatars/" + newFileName;
