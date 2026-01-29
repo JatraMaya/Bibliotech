8eb1daba7353b476c7c20d3bcaed77bd4b93f456
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-29 20:22:31 +0800

    Add Genre Repository

---
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/GenreRepository.java
@@ -0,0 +1,11 @@
+package com.jatramaya.bibliotech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jatramaya.bibliotech.entity.book.GenreEntity;
+
+@Repository
+public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
+
+}
