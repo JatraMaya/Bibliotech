d3ca58a37af4fbefbc158a1bf6c8de08aeabe218
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-29 09:29:58 +0800

    Remove ProfileService and move the method to UserService instead

--- backend/src/main/java/com/jatramaya/bibliotech/service/UserService.java
+++ backend/src/main/java/com/jatramaya/bibliotech/service/UserService.java
@@ -1,16 +1,20 @@
 package com.jatramaya.bibliotech.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jatramaya.bibliotech.dto.UpdateUserDTO;
 import com.jatramaya.bibliotech.entity.user.ProfileEntity;
 import com.jatramaya.bibliotech.entity.user.UserEntity;
 import com.jatramaya.bibliotech.exception.EntityNotFoundException;
+import com.jatramaya.bibliotech.repository.ProfileRepository;
 import com.jatramaya.bibliotech.repository.UserRepository;
 import static com.jatramaya.bibliotech.utils.Utility.hasValue;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
@@ -19,10 +23,42 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
+    @Autowired
+    private ProfileRepository profileRepo;
+
+    @Autowired
+    private FileUploadService uploadService;
+
     public UserEntity getCurrentUser(String username) {
 
         return repo.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
+
+    @Transactional
+    public ProfileEntity createProfile(UserEntity user, String bio, MultipartFile avatar) throws IOException {
+
+        if (profileRepo.existsByUser(user))
+            throw new DataIntegrityViolationException("User profile already exist");
+
+        boolean hasAvatar = avatar != null && !avatar.isEmpty();
+        boolean hasBio = bio != null && !bio.isBlank();
+
+        if (!hasAvatar && !hasBio) {
+            return null;
+        }
+
+        ProfileEntity profile = new ProfileEntity();
+        profile.setUser(user);
+        if (hasAvatar) {
+            String avatarUrl = uploadService.uploadAvatar(avatar);
+            profile.setAvatarUrl(avatarUrl);
+        }
+
+        if (hasBio)
+            profile.setBio(bio);
+
+        return profileRepo.save(profile);
     }
 
     @Transactional
