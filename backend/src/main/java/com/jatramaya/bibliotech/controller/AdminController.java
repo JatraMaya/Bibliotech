00ddd21ed9af5758df1306ac01c426a7c50dded0
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-29 08:21:13 +0800

    Add Create admin and create new user endpoint

--- backend/src/main/java/com/jatramaya/bibliotech/controller/AdminController.java
+++ backend/src/main/java/com/jatramaya/bibliotech/controller/AdminController.java
@@ -6,16 +6,23 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jatramaya.bibliotech.dto.RegisterResponseDTO;
+import com.jatramaya.bibliotech.dto.UserRegisterDTO;
 import com.jatramaya.bibliotech.entity.user.UserEntity;
 import com.jatramaya.bibliotech.service.AdminService;
+import com.jatramaya.bibliotech.service.AuthService;
 import com.jatramaya.bibliotech.service.AuthorService;
 import com.jatramaya.bibliotech.service.UserService;
+
+import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/admin")
@@ -25,6 +32,9 @@ public class AdminController {
 
     @Autowired
     private AdminService adminService;
+
+    @Autowired
+    private AuthService authService;
 
     @Autowired
     private AuthorService authorService;
@@ -41,6 +51,30 @@ public class AdminController {
         return ResponseEntity.ok(Map.of(
                 "status", "Success",
                 "message", "Author data deleted"));
+
+    }
+
+    @PostMapping("/create-admin")
+    public ResponseEntity<?> createNewAdmin(@Valid @RequestBody UserRegisterDTO dto) {
+
+        UserEntity admin = adminService.createAdmin(dto);
+        RegisterResponseDTO response = new RegisterResponseDTO(admin);
+
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "message", "Sucessfully created new ADMIN user.",
+                "admin", response));
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<?> createNewUser(@Valid @RequestBody UserRegisterDTO dto) {
+        UserEntity newUser = authService.register(dto);
+        RegisterResponseDTO response = new RegisterResponseDTO(newUser);
+
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "message", "Success creating a new user",
+                "user", response));
 
     }
 
