412838361d8abb25ad2cc6d7a4aeff19ca751080
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-29 08:19:34 +0800

    Add create new admin and create new user fungsionality to Admin controller

--- backend/src/main/java/com/jatramaya/bibliotech/service/AdminService.java
+++ backend/src/main/java/com/jatramaya/bibliotech/service/AdminService.java
@@ -3,9 +3,11 @@ package com.jatramaya.bibliotech.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jatramaya.bibliotech.dto.UserRegisterDTO;
 import com.jatramaya.bibliotech.entity.user.Role;
 import com.jatramaya.bibliotech.entity.user.UserEntity;
 import com.jatramaya.bibliotech.exception.EntityNotFoundException;
@@ -16,6 +18,9 @@ public class AdminService {
 
     @Autowired
     private UserRepository repo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserEntity assingAdminAccess(UUID id) {
@@ -34,5 +39,21 @@ public class AdminService {
         user.setRole(Role.USER);
 
         return repo.save(user);
+    }
+
+    @Transactional
+    public UserEntity createAdmin(UserRegisterDTO dto) {
+        UserEntity admin = new UserEntity();
+
+        admin.setUsername(dto.getUsername());
+        admin.setEmail(dto.getEmail());
+        admin.setFirstname(dto.getFirstname());
+        if (dto.getLastname() != null && !dto.getLastname().isBlank()) {
+            admin.setLastname(dto.getLastname());
+        }
+        admin.setPassword(passwordEncoder.encode(dto.getPassword()));
+        admin.setRole(Role.ADMIN);
+
+        return repo.save(admin);
     }
 }
