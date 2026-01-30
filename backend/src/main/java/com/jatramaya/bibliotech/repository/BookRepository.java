859b2c65480a9c383d8b3eb485da29a89c295947
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 17:20:08 +0800

    Add method to search book based on author, genre, and tag, add method for book pagination

--- backend/src/main/java/com/jatramaya/bibliotech/repository/BookRepository.java
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/BookRepository.java
@@ -1,5 +1,6 @@
 package com.jatramaya.bibliotech.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -16,6 +17,21 @@ public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
     @Query("SELECT DISTINCT b FROM BookEntity b JOIN b.authors a WHERE b.title = :title AND a.id = :authorId")
     Optional<BookEntity> findByTitleAndAuthor(@Param("title") String title, @Param("authorId") Long authorId);
+
+    @Query("""
+            SELECT DISTINCT b FROM BookEntity b
+            LEFT JOIN b.authors a
+            LEFT JOIN b.genres g
+            LEFT JOIN b.tags t
+            WHERE (:authorIds IS NULL OR a.id IN :authorIds)
+            AND (:genreIds IS NULL OR g.id IN :genreIds)
+            AND (:tagIds IS NULL OR t.id IN :tagIds)
+            """)
+    Page<BookEntity> searchBooks(
+            @Param("authorIds") List<Long> authorIds,
+            @Param("genreIds") List<Long> genreIds,
+            @Param("tagIds") List<Long> tagIds,
+            Pageable pageable);
 
     Page<BookEntity> findAll(Pageable pageable);
 }
