b3f63eaeb4a1b17df8651fbdd2b5137f2293c14c
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 20:45:35 +0800

    Add method to check if book already inside collection

--- backend/src/main/java/com/jatramaya/bibliotech/repository/UserBookRepository.java
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/UserBookRepository.java
@@ -1,5 +1,8 @@
 package com.jatramaya.bibliotech.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import com.jatramaya.bibliotech.entity.user.UserBookEntity;
 @Repository
 public interface UserBookRepository extends JpaRepository<UserBookEntity, Long> {
 
+    Optional<UserBookEntity> findByUserIdAndBookId(UUID userId, Long bookId);
+
+    boolean existsByUserIdAndBookId(UUID userId, Long bookId);
 }
