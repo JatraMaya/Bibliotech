2d64ebac89d269a22eaa8e8fc5e3c5786862e17b
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-31 07:58:15 +0800

    Add DTO for updating book review

---
+++ backend/src/main/java/com/jatramaya/bibliotech/dto/ReviewDTO.java
@@ -0,0 +1,13 @@
+package com.jatramaya.bibliotech.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewDTO {
+
+    private String review;
+}
