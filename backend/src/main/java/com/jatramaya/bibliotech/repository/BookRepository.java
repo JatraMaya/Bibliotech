7cfd47363cb725301209eb2cbddb00454cc01f30
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 10:52:52 +0800

    Add method to lookup book based on title and author id

--- backend/src/main/java/com/jatramaya/bibliotech/repository/BookRepository.java
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/BookRepository.java
@@ -1,6 +1,10 @@
 package com.jatramaya.bibliotech.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.jatramaya.bibliotech.entity.book.BookEntity;
@@ -8,4 +12,6 @@ import com.jatramaya.bibliotech.entity.book.BookEntity;
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
+    @Query("SELECT DISTINCT b FROM BookEntity b JOIN b.authors a WHERE b.title = :title AND a.id = :authorId")
+    Optional<BookEntity> findByTitleAndAuthor(@Param("title") String title, @Param("authorId") Long authorId);
 }
