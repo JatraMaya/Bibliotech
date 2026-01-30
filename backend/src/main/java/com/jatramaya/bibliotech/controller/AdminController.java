5dc241f914a9ac0bd819f3ea6b759ccbcc2d392e
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 17:38:17 +0800

    remove Tag delet endpoint to admin instead

--- backend/src/main/java/com/jatramaya/bibliotech/controller/AdminController.java
+++ backend/src/main/java/com/jatramaya/bibliotech/controller/AdminController.java
@@ -17,6 +17,7 @@ import com.jatramaya.bibliotech.entity.user.UserEntity;
 import com.jatramaya.bibliotech.service.AdminService;
 import com.jatramaya.bibliotech.service.AuthService;
 import com.jatramaya.bibliotech.service.AuthorService;
+import com.jatramaya.bibliotech.service.TagService;
 import com.jatramaya.bibliotech.service.UserService;
 
 import jakarta.validation.Valid;
@@ -34,158 +35,171 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final UserService userService;
+        private final UserService userService;
 
-    @Autowired
-    private AdminService adminService;
+        @Autowired
+        private AdminService adminService;
 
-    @Autowired
-    private AuthService authService;
+        @Autowired
+        private AuthService authService;
 
-    @Autowired
-    private AuthorService authorService;
+        @Autowired
+        private AuthorService authorService;
 
-    AdminController(UserService userService) {
-        this.userService = userService;
-    }
+        @Autowired
+        private TagService tagService;
 
-    /// Author Related endpoint
-
-    @PostMapping("/author")
-    public ResponseEntity<?> createNewAuthor(
-            @Valid @ModelAttribute AddAuthorDTO dto,
-            @RequestParam(required = false) MultipartFile profilePic) throws IOException {
-
-        AuthorEntity author = authorService.createAuthor(dto, profilePic);
-
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Crated new Author" + author.getName(),
-                "author", author));
-    }
-
-    @PutMapping("/author/{id}")
-    public ResponseEntity<?> updateAuthor(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) MultipartFile img,
-            @PathVariable Long id) throws IOException {
-        boolean updated = authorService.updateAuthor(id, name, img);
-
-        if (updated) {
-            return ResponseEntity.ok(Map.of(
-                    "status", "success",
-                    "message", "Successfully update author data"));
+        AdminController(UserService userService) {
+                this.userService = userService;
         }
 
-        return ResponseEntity.ok(Map.of(
-                "status", "success",
-                "message", "author data not updated"));
-    }
+        /// Author Related endpoint
 
-    @DeleteMapping("/author/{id}")
-    public ResponseEntity<Map<String, String>> deleteAuthor(@PathVariable Long id) throws IOException {
+        @PostMapping("/author")
+        public ResponseEntity<?> createNewAuthor(
+                        @Valid @ModelAttribute AddAuthorDTO dto,
+                        @RequestParam(required = false) MultipartFile profilePic) throws IOException {
 
-        authorService.deleteAuthor(id);
+                AuthorEntity author = authorService.createAuthor(dto, profilePic);
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Author data deleted"));
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Crated new Author" + author.getName(),
+                                "author", author));
+        }
 
-    }
+        @PutMapping("/author/{id}")
+        public ResponseEntity<?> updateAuthor(
+                        @RequestParam(required = false) String name,
+                        @RequestParam(required = false) MultipartFile img,
+                        @PathVariable Long id) throws IOException {
+                boolean updated = authorService.updateAuthor(id, name, img);
 
-    @PutMapping("/author/img/{id}")
-    public ResponseEntity<?> addAuthorPicture(
-            @PathVariable Long id, @RequestParam(required = true) MultipartFile image) throws IOException {
+                if (updated) {
+                        return ResponseEntity.ok(Map.of(
+                                        "status", "success",
+                                        "message", "Successfully update author data"));
+                }
 
-        AuthorEntity author = authorService.updateImg(id, image);
+                return ResponseEntity.ok(Map.of(
+                                "status", "success",
+                                "message", "author data not updated"));
+        }
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Add profile image for author " + author.getName()));
-    }
+        @DeleteMapping("/author/{id}")
+        public ResponseEntity<Map<String, String>> deleteAuthor(@PathVariable Long id) throws IOException {
 
-    @DeleteMapping("/author/img/{id}")
-    public ResponseEntity<?> deleteAuthorImage(@PathVariable Long id) throws IOException {
-        AuthorEntity author = authorService.deleteImg(id);
+                authorService.deleteAuthor(id);
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Success deleting img profile for author " + author.getName()));
-    }
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Author data deleted"));
 
-    /// Role Realted endpoint
+        }
 
-    @PostMapping("/create-admin")
-    public ResponseEntity<?> createNewAdmin(@Valid @RequestBody UserRegisterDTO dto) {
+        @PutMapping("/author/img/{id}")
+        public ResponseEntity<?> addAuthorPicture(
+                        @PathVariable Long id, @RequestParam(required = true) MultipartFile image) throws IOException {
 
-        UserEntity admin = adminService.createAdmin(dto);
-        RegisterResponseDTO response = new RegisterResponseDTO(admin);
+                AuthorEntity author = authorService.updateImg(id, image);
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Sucessfully created new ADMIN user.",
-                "admin", response));
-    }
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Add profile image for author " + author.getName()));
+        }
 
-    @PostMapping("/create-user")
-    public ResponseEntity<?> createNewUser(@Valid @RequestBody UserRegisterDTO dto) {
-        UserEntity newUser = authService.register(dto);
-        RegisterResponseDTO response = new RegisterResponseDTO(newUser);
+        @DeleteMapping("/author/img/{id}")
+        public ResponseEntity<?> deleteAuthorImage(@PathVariable Long id) throws IOException {
+                AuthorEntity author = authorService.deleteImg(id);
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Success creating a new user",
-                "user", response));
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Success deleting img profile for author " + author.getName()));
+        }
 
-    }
+        /// Role Realted endpoint
 
-    /// User related endpoint
+        @PostMapping("/create-admin")
+        public ResponseEntity<?> createNewAdmin(@Valid @RequestBody UserRegisterDTO dto) {
 
-    @PutMapping("/user/{id}/assign")
-    public ResponseEntity<?> assingAdminFromUser(@PathVariable UUID id) {
+                UserEntity admin = adminService.createAdmin(dto);
+                RegisterResponseDTO response = new RegisterResponseDTO(admin);
 
-        UserEntity newAdmin = adminService.assingAdminAccess(id);
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Sucessfully created new ADMIN user.",
+                                "admin", response));
+        }
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Success Assign user with Admin privilage",
-                "admin", Map.of(
-                        "username", newAdmin.getUsername(),
-                        "firstname", newAdmin.getFirstname(),
-                        "email", newAdmin.getEmail(),
-                        "role", newAdmin.getRole().name()
+        @PostMapping("/create-user")
+        public ResponseEntity<?> createNewUser(@Valid @RequestBody UserRegisterDTO dto) {
+                UserEntity newUser = authService.register(dto);
+                RegisterResponseDTO response = new RegisterResponseDTO(newUser);
 
-                )
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Success creating a new user",
+                                "user", response));
 
-        ));
-    }
+        }
 
-    @PutMapping("/user/{id}/revoke")
-    public ResponseEntity<?> revokeAdminFromUser(@PathVariable UUID id) {
+        /// User related endpoint
 
-        UserEntity newAdmin = adminService.revokeAdminAccess(id);
+        @PutMapping("/user/{id}/assign")
+        public ResponseEntity<?> assingAdminFromUser(@PathVariable UUID id) {
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "Success revoke admin access",
-                "admin", Map.of(
-                        "username", newAdmin.getUsername(),
-                        "firstname", newAdmin.getFirstname(),
-                        "email", newAdmin.getEmail(),
-                        "role", newAdmin.getRole().name()
+                UserEntity newAdmin = adminService.assingAdminAccess(id);
 
-                )
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Success Assign user with Admin privilage",
+                                "admin", Map.of(
+                                                "username", newAdmin.getUsername(),
+                                                "firstname", newAdmin.getFirstname(),
+                                                "email", newAdmin.getEmail(),
+                                                "role", newAdmin.getRole().name()
 
-        ));
-    }
+                                )
 
-    @DeleteMapping("/user/{id}/delete")
-    public ResponseEntity<?> deleteUser(@PathVariable UUID id) throws IOException {
-        userService.deleteUserDataById(id);
+                ));
+        }
 
-        return ResponseEntity.ok(Map.of(
-                "status", "Success",
-                "message", "User deleted successfully"));
-    }
+        @PutMapping("/user/{id}/revoke")
+        public ResponseEntity<?> revokeAdminFromUser(@PathVariable UUID id) {
+
+                UserEntity newAdmin = adminService.revokeAdminAccess(id);
+
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "Success revoke admin access",
+                                "admin", Map.of(
+                                                "username", newAdmin.getUsername(),
+                                                "firstname", newAdmin.getFirstname(),
+                                                "email", newAdmin.getEmail(),
+                                                "role", newAdmin.getRole().name()
+
+                                )
+
+                ));
+        }
+
+        @DeleteMapping("/user/{id}/delete")
+        public ResponseEntity<?> deleteUser(@PathVariable UUID id) throws IOException {
+                userService.deleteUserDataById(id);
+
+                return ResponseEntity.ok(Map.of(
+                                "status", "Success",
+                                "message", "User deleted successfully"));
+        }
+
+        // Tag and Genre Related endpoint
+        @DeleteMapping("/tag/{id}")
+        public ResponseEntity<?> deleteTag(@PathVariable Long id) {
+                tagService.deleteTag(id);
+
+                return ResponseEntity.ok(Map.of(
+                                "status", "success",
+                                "message", "Tag deleted"));
+        }
 
 }
--- backend/src/main/java/com/jatramaya/bibliotech/controller/TagController.java
+++ backend/src/main/java/com/jatramaya/bibliotech/controller/TagController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -68,15 +67,6 @@ public class TagController {
                 "totalItems", pageData.getTotalElements(),
                 "totalPages", pageData.getTotalPages()));
 
-    }
-
-    @DeleteMapping
-    public ResponseEntity<?> deleteTag(@PathVariable Long id) {
-        service.deleteTag(id);
-
-        return ResponseEntity.ok(Map.of(
-                "status", "success",
-                "message", "Tag deleted"));
     }
 
 }
