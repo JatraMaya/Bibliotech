b66bf39293239d4bea4b77c3367731e630582a56
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 18:46:54 +0800

    Add new method variant for hasValue

--- backend/src/main/java/com/jatramaya/bibliotech/utils/Utility.java
+++ backend/src/main/java/com/jatramaya/bibliotech/utils/Utility.java
@@ -1,8 +1,20 @@
 package com.jatramaya.bibliotech.utils;
 
+import java.util.Collection;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public class Utility {
 
     public static boolean hasValue(String value) {
         return value != null && !value.isBlank();
+    }
+
+    public static boolean hasValue(Collection<?> collection) {
+        return collection != null && !collection.isEmpty();
+    }
+
+    public static boolean hasValue(MultipartFile file) {
+        return file != null && !file.isEmpty();
     }
 }
