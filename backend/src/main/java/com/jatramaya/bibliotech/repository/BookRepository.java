9a6426de6be30e9099512896505964354ef2502b
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 16:32:36 +0800

    Add method for Pagination

--- backend/src/main/java/com/jatramaya/bibliotech/repository/BookRepository.java
+++ backend/src/main/java/com/jatramaya/bibliotech/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.jatramaya.bibliotech.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,6 @@ public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
     @Query("SELECT DISTINCT b FROM BookEntity b JOIN b.authors a WHERE b.title = :title AND a.id = :authorId")
     Optional<BookEntity> findByTitleAndAuthor(@Param("title") String title, @Param("authorId") Long authorId);
+
+    Page<BookEntity> findAll(Pageable pageable);
 }
