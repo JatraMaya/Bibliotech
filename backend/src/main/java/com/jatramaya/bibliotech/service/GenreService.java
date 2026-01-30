c03cad428aaf47895bc2c66c96b5f1c071e0a33a
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 17:44:17 +0800

    Add delete method

--- backend/src/main/java/com/jatramaya/bibliotech/service/GenreService.java
+++ backend/src/main/java/com/jatramaya/bibliotech/service/GenreService.java
@@ -3,8 +3,10 @@ package com.jatramaya.bibliotech.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jatramaya.bibliotech.entity.book.GenreEntity;
+import com.jatramaya.bibliotech.exception.EntityNotFoundException;
 import com.jatramaya.bibliotech.repository.GenreRepository;
 
 @Service
@@ -12,6 +14,7 @@ public class GenreService {
     @Autowired
     private GenreRepository repo;
 
+    @Transactional
     public GenreEntity createNewGenre(String name) {
 
         String nameLowerCase = name.toLowerCase();
@@ -23,6 +26,12 @@ public class GenreService {
         genre.setName(nameLowerCase);
 
         return repo.save(genre);
+    }
+
+    public void deleteGenre(Long id) {
+        GenreEntity tag = repo.findById(id).orElseThrow(() -> new EntityNotFoundException("Tag not found"));
+
+        repo.delete(tag);
     }
 
 }
