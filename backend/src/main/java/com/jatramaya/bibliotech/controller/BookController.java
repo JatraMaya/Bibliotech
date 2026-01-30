c368a03400dd1345a96b0fd3dbb612dff6bd03dd
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-30 16:33:21 +0800

    Add endpoint to get all books and get book by id

--- backend/src/main/java/com/jatramaya/bibliotech/controller/BookController.java
+++ backend/src/main/java/com/jatramaya/bibliotech/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.jatramaya.bibliotech.dto.CreateBookDTO;
 import com.jatramaya.bibliotech.dto.CreateBookResponseDTO;
+import com.jatramaya.bibliotech.entity.book.BookEntity;
 import com.jatramaya.bibliotech.service.BookService;
 
 import jakarta.validation.Valid;
@@ -15,8 +16,11 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
@@ -38,4 +42,35 @@ public class BookController {
                 "message", "Add new Book succesfully",
                 "book", book));
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "ASC") String direction) {
+
+        Page<CreateBookResponseDTO> pageData = service.getAll(page, size, sortBy, direction);
+        return ResponseEntity.ok(Map.of(
+                "status", "Success",
+                "data", pageData.getContent(),
+                "currentPage", pageData.getNumber(),
+                "totalItems", pageData.getTotalElements(),
+                "totalPages", pageData.getTotalPages()));
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getBook(@PathVariable Long id) {
+        BookEntity book = service.getById(id);
+
+        CreateBookResponseDTO response = new CreateBookResponseDTO(book);
+
+        return ResponseEntity.ok(Map.of(
+            "status", "Success",
+            "message", "Succes get book data",
+            "book", response
+        ));
+    }
+    
 }
