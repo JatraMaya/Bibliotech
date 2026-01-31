d42b5d1274f5a699f4e1b08de0971362186fd556
JatraMaya <ahmad.f.iksan@gmail.com>
2026-01-31 08:00:56 +0800

    Add various method: - Add book to user's collection - Remove book from user's collection - Remove book from user's collection with userID param - Update reading status for book in user's collection - Update review for book in user's collection

--- backend/src/main/java/com/jatramaya/bibliotech/service/UserBookService.java
+++ backend/src/main/java/com/jatramaya/bibliotech/service/UserBookService.java
@@ -6,11 +6,16 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jatramaya.bibliotech.dto.AddBookCollectionDTO;
+import com.jatramaya.bibliotech.dto.BookCollectionDTO;
 import com.jatramaya.bibliotech.entity.book.BookEntity;
+import com.jatramaya.bibliotech.entity.book.BookStatus;
 import com.jatramaya.bibliotech.entity.user.UserBookEntity;
 import com.jatramaya.bibliotech.entity.user.UserEntity;
 import com.jatramaya.bibliotech.exception.EntityNotFoundException;
@@ -48,11 +53,68 @@ public class UserBookService {
         UserEntity user = userRepo.findById(userId)
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
         collection.setUser(user);
-        collection.setComment(dto.getComment());
+        collection.setComment(dto.getComment().toLowerCase());
         if (hasValue(dto.getReadingStatus().name())) {
             collection.setReadingStatus(dto.getReadingStatus());
         }
 
         return repo.save(collection);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<BookCollectionDTO> getAllCollection(UUID userId, int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<UserBookEntity> books = repo.findAllByUserId(userId, pageable);
+
+        return books.map(book -> {
+            return new BookCollectionDTO(book);
+        });
+    }
+
+    @Transactional
+    public boolean updateReadingStatus(Long id, BookStatus bookStatus) {
+
+        UserBookEntity book = repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No book collection found"));
+
+        boolean updated = false;
+
+        if (book.getReadingStatus().name() != bookStatus.name()) {
+            updated = true;
+            book.setReadingStatus(bookStatus);
+        }
+        return updated;
+    }
+
+    @Transactional
+    public boolean updateComment(Long id, String comment) {
+        UserBookEntity book = repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No book collection found"));
+
+        boolean updated = false;
+
+        if (book.getComment() != comment || !comment.isBlank()) {
+            book.setComment(comment);
+            repo.save(book);
+            updated = true;
+        }
+
+        return updated;
+    }
+
+    @Transactional
+    public void removeFromCollection(long id) {
+        UserBookEntity book = repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No book collection found"));
+
+        repo.delete(book);
+    }
+
+    @Transactional
+    public void removeFromUserCollection(UUID userId, Long id) {
+        UserBookEntity book = repo.findByUserIdAndId(userId, id)
+                .orElseThrow(() -> new EntityNotFoundException("No Book found from user collection"));
+        repo.delete(book);
     }
 }
