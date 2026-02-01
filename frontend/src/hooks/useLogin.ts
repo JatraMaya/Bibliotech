3621f76684c4a248a43acece23fe7299cbb09e7e
JatraMaya <ahmad.f.iksan@gmail.com>
2026-02-01 08:54:12 +0800

    Refactor for readability and add useToastStore hook to handle toast

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,33 +1,34 @@
 import { useMutation } from '@tanstack/react-query';
 import { useAuthStore } from '../store/authStore';
-
-import type { LoginRequest } from '../types/authType';
-
 import { login as apiLogin, getUserData } from '../api/auth';
 import { useNavigate } from 'react-router-dom';
+import { useToastStore } from '../store/toastStore';
+import type { LoginRequest } from '../types/authType';
 
 export const useLogin = () => {
   const login = useAuthStore((state) => state.login);
   const navigate = useNavigate();
+  const showToast = useToastStore((state) => state.show);
 
   return useMutation({
     mutationFn: async (data: LoginRequest) => {
       const loginRes = await apiLogin(data);
       const userInfo = await getUserData(loginRes.token);
 
-      return { token: loginRes.token, user: userInfo };
+      return {
+        token: loginRes.token,
+        user: userInfo,
+        message: loginRes.message,
+      };
     },
     onSuccess: (data) => {
-      console.log('RESPONSE FROM API: ', data.user);
+      showToast('success', data.message);
       login(data.user, data.token);
 
-      if (data.user.role == 'ADMIN') {
-        console.log("NAVIGATE TO ADMIN")
-        navigate('/admin');
-      } else {
-        console.log("NAVIGATE TO DASHBOARD")
-        navigate('/dashboard');
-      }
+      navigate(data.user.role === 'ADMIN' ? '/admin' : '/dashboard');
+    },
+    onError: (error: any) => {
+      showToast('error', error.message);
     },
   });
 };
