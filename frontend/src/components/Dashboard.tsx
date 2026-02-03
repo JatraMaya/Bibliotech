99ec7d3f8b6ce754fe69c54c9f895be8f4790910
JatraMaya <ahmad.f.iksan@gmail.com>
2026-02-03 14:42:45 +0800

    Add new Book collection component

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,9 +1,38 @@
+import { useGetBookCollection } from '../hooks/useGetBookCollection';
+import { ReadingStatus } from '../types/BookType';
+import BooksCollection from './BookCollection';
 import Navbar from './Navbar';
 
 export default function Dashboard() {
+  const { data, isLoading, isError, error } = useGetBookCollection(0, 10);
+  if (isLoading) {
+    return (
+      <>
+        <Navbar />
+        <div className="flex justify-center items-center min-h-[60vh]">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <Navbar />
+        <div className="flex justify-center items-center min-h-[60vh]">
+          <div className="alert alert-error">
+            <span>{error.message}</span>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Navbar />
+      <BooksCollection books={data?.data || []} />
     </>
   );
 }
