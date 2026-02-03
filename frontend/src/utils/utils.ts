e06905bb835bbc5d4be29df2293292befc2ba158
JatraMaya <ahmad.f.iksan@gmail.com>
2026-02-03 14:43:32 +0800

    Add utility method for Error Response based on status code

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -11,3 +11,27 @@ export function errorToMessage(message: unknown): string {
 
   return 'Unknown error';
 }
+
+export function getApiPath(): string {
+  return import.meta.env.VITE_API_URL;
+}
+
+export function fullnameToInitial(fullname: string) {
+  return fullname
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase())
+    .join('');
+}
+
+export function errorWithStatusCode(
+  message: string,
+  statusCode: number,
+  defaultMessage = '',
+): never {
+  const err = new Error(message || defaultMessage) as Error & {
+    statusCode: number;
+  };
+
+  err.statusCode = statusCode;
+  throw err;
+}
