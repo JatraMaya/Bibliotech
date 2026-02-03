c42e1c01563f2e37532dd5467c5391f7978e0394
JatraMaya <ahmad.f.iksan@gmail.com>
2026-02-03 14:49:22 +0800

    Refactor type for API response

--- frontend/src/types/UserType.ts
+++ frontend/src/types/UserType.ts
@@ -3,6 +3,16 @@ export interface User {
   username: string;
   fullname: string;
   avatarUrl: string | null;
-  bio: string | null;
-  role: string;
+  bio?: string | null;
+  role?: string;
+}
+
+export interface AddUserProfile {
+  bio: string;
+}
+
+export interface EditUserProfile {
+  firstname?: string | null;
+  lastname?: string | null;
+  bio?: string | null;
 }
