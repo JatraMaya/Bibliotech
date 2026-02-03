363c2a642413366536a583ab407b6b19dee73813
JatraMaya <ahmad.f.iksan@gmail.com>
2026-02-03 14:48:37 +0800

    Replace authStore method login -> setSession to describe function better

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import type { AuthState } from '../types/authType';
 import { persist } from 'zustand/middleware';
+import type { User } from '../types/UserType';
 
 export const useAuthStore = create<AuthState>()(
   persist(
@@ -8,8 +9,12 @@ export const useAuthStore = create<AuthState>()(
       user: null,
       token: null,
       isAuthenticated: false,
-      login: (user, token) => set({ user, token, isAuthenticated: true }),
+      setSession: (user, token) => set({ user, token, isAuthenticated: true }),
       logout: () => set({ user: null, token: null, isAuthenticated: false }),
+      update: (partial: Partial<User>) =>
+        set((state) => ({
+          user: state?.user ? { ...state.user, ...partial } : null,
+        })),
     }),
     {
       name: 'auth-storage',
